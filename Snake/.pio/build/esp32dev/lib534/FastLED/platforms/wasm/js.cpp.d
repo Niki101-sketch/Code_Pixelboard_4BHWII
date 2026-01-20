2d1c3dca446417b02b2377be850e3194db5a13f1
Nikolas Blättner <niki.craft8873@outlook.de>
2026-01-20 13:06:46 +0100

    Snake_Code_Test_Nikolas

---
+++ Snake/.pio/build/esp32dev/lib534/FastLED/platforms/wasm/js.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/esp32dev/lib534/FastLED/platforms/wasm/js.cpp.o: \
+ .pio/libdeps/esp32dev/FastLED/src/platforms/wasm/js.cpp
---
+++ Snake/.pio/build/esp32dev/lib534/FastLED/platforms/wasm/js_bindings.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/esp32dev/lib534/FastLED/platforms/wasm/js_bindings.cpp.o: \
+ .pio/libdeps/esp32dev/FastLED/src/platforms/wasm/js_bindings.cpp
